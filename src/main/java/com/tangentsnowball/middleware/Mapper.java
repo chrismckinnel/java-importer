f83cab0d9ed48499ef2b9d45e2e683fe12d8bb04
Chris McKinnel <chrismckinnel@gmail.com>
2015-02-03 19:27:25 +0000

    Add middleware

---
+++ src/main/java/com/tangentsnowball/middleware/Mapper.java
@@ -0,0 +1,11 @@
+package com.tangentsnowball.middleware;
+
+import java.util.function.Function;
+
+import com.tangentsnowball.entity.Book;
+
+public class Mapper implements Function<String, Book> {
+	public Book apply(String element) {
+		return null;
+	}
+}
